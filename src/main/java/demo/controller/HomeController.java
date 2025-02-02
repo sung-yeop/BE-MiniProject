277ab0ea4e6293fbc9ad4d49bad67befdc1c8732
sung-yeop <jjabc9459@naver.com>
2025-02-02 21:55:25 +0900

    style : 전체 페이지 스타일링 수정

--- src/main/java/demo/controller/HomeController.java
+++ src/main/java/demo/controller/HomeController.java
@@ -1,8 +1,10 @@
 package demo.controller;
 
 import demo.dto.ExerciseDto;
+import demo.dto.RoutineDto;
 import demo.dto.UserProfileDto;
 import demo.service.exercise.ExerciseService;
+import demo.service.routine.RoutineService;
 import demo.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,14 +20,18 @@ public class HomeController {
     private UserService userService;
     @Autowired
     private ExerciseService exerciseService;
+    @Autowired
+    private RoutineService routineService;
 
     @GetMapping("/home")
     public ModelAndView home(@RequestParam("userId") Long userId) {
         ModelAndView mv = new ModelAndView("/home/home");
         UserProfileDto userProfileDto = userService.selectUserDetail(userId);
         List<ExerciseDto> exerciseList = exerciseService.getUserExercise(userId);
+        List<RoutineDto> routineList = routineService.getRoutineList(userId);
         mv.addObject("exerciseList", exerciseList);
         mv.addObject("userProfileDto", userProfileDto);
+        mv.addObject("routineList", routineList);
         return mv;
     }
 }
